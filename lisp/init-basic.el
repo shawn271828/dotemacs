b1753196d3569c6bd5723c908e335d7c021b8b7d
Shuo Tan <ts@vsoft.io>
2018-06-12 15:34:07 +0800

    start server mode default

--- lisp/init-basic.el
+++ lisp/init-basic.el
@@ -75,9 +75,9 @@
   (add-to-list 'recentf-exclude '("/tmp/" "/ssh:")))
 
 ;; Start server
-;; (use-package server
-;;   :ensure nil
-;;   :init (add-hook 'after-init-hook #'server-mode))
+(use-package server
+  :ensure nil
+  :init (add-hook 'after-init-hook #'server-mode))
 
 (provide 'init-basic)
 
