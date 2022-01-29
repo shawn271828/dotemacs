bc8efe550e9b57abcea555291169fc86c3eeb68c
Tan Shuo <tanshuo.ts@alibaba-inc.com>
2022-01-29 14:46:01 +0800

    simplify key binding for layout manager

--- lisp/init-window.el
+++ lisp/init-window.el
@@ -40,7 +40,7 @@
 (use-package ace-window
   :demand
   :init (add-hook 'after-init-hook #'winner-mode)
-  :bind (("<f5>" . hydra-layout-manager/body)
+  :bind (("M-O" . hydra-layout-manager/body)
          ("M-o" . ace-window))
   :config
   (use-package windmove :demand)
